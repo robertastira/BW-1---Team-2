25685524f44e934f966445cc802012770e1de8c3
hamed_jemmali <hamed.jemmali2@gmail.com>
2023-12-11 17:35:26 +0100

    incomplete feedback page

---
+++ JavaScript/feedback.js
@@ -0,0 +1,22 @@
+// const star=function(){
+
+// for(i=0;i<10;i++){
+// const image =document.createElement("img") 
+// image.setAttribute("src",'../Css/Assets/star.svg')
+// image.addEventListener('click',function(){
+   
+    
+//     image.classList.add('s')
+ 
+
+
+// }
+
+// )
+// const emptyDiv=document.getElementById("stars") 
+// emptyDiv.appendChild(image)
+// }
+
+
+// }
+// // star()
